e49734766af536e458a67fb317a88c6f943b905c
tanacloud <tanacloudbrasil@gmail.com>
2024-11-18 09:26:49 -0300

    Add files via upload

---
+++ spring-boot-hello-world-example/src/main/java/com/helloworld/controller/HelloWorldController.java
@@ -0,0 +1,12 @@
+package com.helloworld.controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class HelloWorldController 
+{
+@RequestMapping("/")
+public String hello() 
+{
+return "<h1> Congratulations. This is an update of Spring Boot Application. </h1>";
+}
+}
